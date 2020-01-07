132e2dca33afafe1ac3fb5ed892579ae9d3f2206
Sam Beardman <sam.beardman@powerco.co.nz>
2020-01-07 14:03:52 +1300

    Remove incorrect outputs

--- outputs.tf
+++ outputs.tf
@@ -2,20 +2,3 @@ output "id" {
     description = "The automation account resource id"
     value = azurerm_automation_account.auto_account.id
 }
-
-output "endpoint" {
-    description = "The automation account endpoint"
-    value = azurerm_automation_account.auto_account.endpoint
-}
-
-output "primary_key" {
-    description = "The automation account primary key"
-    value = azurerm_automation_account.auto_account.primary_key
-    sensitive = true
-}
-
-output "secondary_key" {
-    description = "The automation account secondary key"
-    value = azurerm_automation_account.auto_account.secondary_key
-    sensitive = true
-}
