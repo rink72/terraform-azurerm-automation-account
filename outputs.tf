6dae6c2994a70d3926e9704071797b3b87491ae3
Sam <beardmans72@gmail.com>
2020-01-22 22:13:18 +1300

    Add sensitive parameter

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,11 @@ output "endpoint" {
 output "primary_access_key" {
   description = "The automation account primary key"
   value       = azurerm_automation_account.auto_account.dsc_primary_access_key
+  sensitive   = true
 }
 
 output "secondary_access_key" {
   description = "The automation account secondary key"
   value       = azurerm_automation_account.auto_account.dsc_secondary_access_key
+  sensitive   = true
 }
