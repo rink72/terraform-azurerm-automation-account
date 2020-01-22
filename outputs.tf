a567af8fe3cb686bf92ed04d8f21d7f390886c72
Sam <beardmans72@gmail.com>
2020-01-22 21:53:29 +1300

    Add extra outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "id" {
-    description = "The automation account resource id"
-    value = azurerm_automation_account.auto_account.id
+  description = "The automation account resource id"
+  value       = azurerm_automation_account.auto_account.id
+}
+
+output "endpint" {
+  description = "The automation account endpoint url"
+  value       = azurerm_automation_account.auto_account.dsc_server_endpoint
+}
+
+output "primary_access_key" {
+  description = "The automation account primary key"
+  value       = azurerm_automation_account.auto_account.dsc_primary_access_key
+}
+
+output "secondary_access_key" {
+  description = "The automation account secondary key"
+  value       = azurerm_automation_account.auto_account.dsc_secondary_access_key
 }
